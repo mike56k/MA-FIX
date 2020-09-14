5f6aea8239e2c141a29b8df257e70ec03362be8e
mike56k <miha.is@mail.ru>
2020-09-14 13:11:07 +0300

    Updates

---
+++ precache-manifest.73ffa583c3fe2ad934d6f891652737c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13426ef64bedfccd90272a04f34c6513",
+    "url": "/MA-FIX/index.html"
+  },
+  {
+    "revision": "8068fed5d0f53b94752b",
+    "url": "/MA-FIX/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "8068fed5d0f53b94752b",
+    "url": "/MA-FIX/static/js/2.4943a2ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MA-FIX/static/js/2.4943a2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb9f443063b5ab47c87",
+    "url": "/MA-FIX/static/js/main.9426daaf.chunk.js"
+  },
+  {
+    "revision": "5e8cb4db9e20cd20ffac",
+    "url": "/MA-FIX/static/js/runtime-main.bd215490.js"
+  }
+]);
